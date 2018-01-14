804317eec24367f1821dff2138810134d5b80025
Anthony Santa <anthonysanta95@gmail.com>
2018-01-14 21:45:24 +0000

    Fix default createdAt date

--- models/comment.js
+++ models/comment.js
@@ -10,7 +10,7 @@ var commentSchema = mongoose.Schema({
         },
         username: String
     },
-    createdAt: { type: Date, default: "2018-1-1" }
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
