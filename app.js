4282e96210eec8a630557881c81386070d9a3632
Anthony Santa <anthonysanta95@gmail.com>
2018-01-14 21:14:41 +0000

    Add timestamps with Moment JS

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
+app.locals.moment = require("moment");
+
 // Passport configuration
 app.use(require("express-session")({
     secret: "e=mc^2",
